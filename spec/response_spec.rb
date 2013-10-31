d00f513a0db8de09448d8fc840e3b5d3a027a0e3
Ben Bean <bbean86@gmail.com>
2013-10-31 12:33:56 -0500

    Added some test coverage to response

--- spec/response_spec.rb
+++ spec/response_spec.rb
@@ -4,14 +4,14 @@ describe BypassStoredValue::Response do
   it "should respond to successful?" do
     BypassStoredValue::Response.any_instance.stub(:parse)
     response_stub = double(body: {status_code: 0})
-    response = BypassStoredValue::Response.new(response_stub, "stadis_charge")
+    response = BypassStoredValue::Response.new(response_stub, "stadis_account_charge")
     response.should respond_to(:successful?)
   end
 
   it "should call #parse when initialized" do
     response_stub = double(body: {status_code: 0})
     BypassStoredValue::Response.any_instance.should_receive(:parse)
-    BypassStoredValue::Response.new(response_stub, "stadis_charge")
+    BypassStoredValue::Response.new(response_stub, "stadis_account_charge")
   end
 
   context "#parse" do
@@ -61,40 +61,74 @@ describe BypassStoredValue::Response do
     it "should return an empty response if it receives a blank response from the server" do
       response_stub = double(body: nil)
       BypassStoredValue::Response.any_instance.should_receive(:empty_response)
-      response = BypassStoredValue::Response.new(response_stub, "stadis_charge")
+      response = BypassStoredValue::Response.new(response_stub, "stadis_account_charge")
     end
 
     it "should return an unsuccessful response if it receives a blank response from the server" do
       response_stub = double(body: nil)
-      response = BypassStoredValue::Response.new(response_stub, "stadis_charge")
+      response = BypassStoredValue::Response.new(response_stub, "stadis_account_charge")
       response.successful?.should == false
     end
   end
 
+  context "#build_stadis_reload_response" do
+    it "should build an appropriate hash based on the given response" do
+      response_stub = double(body: {
+        :reload_gift_card_response => {
+          :reload_gift_card_result => {
+            :return_message => {
+              :return_code => "-2",
+              :message => "Ticket not found or event not open."},
+            :stadis_reply => {
+              :reference_number => "GCLOAD",
+              :stadis_authorization_id => nil,
+              :tender_status_code => "0",
+              :tender_status_message => nil,
+              :account_status_code => "2",
+              :account_status_message => "Customer status unknown.;;0;0;0;0;0;;;",
+              :charged_amount => "0",
+              :remaining_amount => "0"}},
+        :@xmlns => "http://www.STADIS.com/"}})
+        response = BypassStoredValue::Response.new(response_stub, "stadis_reload")
+        response.result.should == {status_code: -2, authentication_token: nil, charged_amount: 0.0, remaining_balance: 0.0}
+    end
+    
+  end
+
   context "#successful?" do
     before do
-      @response_stub = double(body: {})
+      @successful_response_stub = double(body: {
+        stadis_account_charge_response: { 
+          stadis_account_charge_result: { 
+            return_message: { 
+              return_code: "0"}, 
+            stadis_reply: {
+              stadis_authorization_id: 'AUTH_ID', 
+              charged_amount: 2.00, 
+              remaining_amount: 5.00}}}})
+      @failed_response_stub = double(body: {
+        stadis_account_charge_response: { 
+          stadis_account_charge_result: { 
+            return_message: { 
+              return_code: "-1"}, 
+            stadis_reply: {
+              stadis_authorization_id: 'AUTH_ID', 
+              charged_amount: 0.00, 
+              remaining_amount: 0.00}}}})
     end
 
     it "should return true for status codes >= 0" do
-      BypassStoredValue::Response.any_instance.stub(:parse)
-      response = BypassStoredValue::Response.new(@response_stub, "stadis_charge")
-      response.result[:status_code] = 0
-      response.successful?.should == true
-      response = BypassStoredValue::Response.new(@response_stub, "stadis_charge")
-      response.result[:status_code] = 1
+      response = BypassStoredValue::Response.new(@successful_response_stub, "stadis_account_charge")
       response.successful?.should == true
     end
 
     it "should return false for status codes < 0" do
-      BypassStoredValue::Response.any_instance.stub(:parse)
-      response = BypassStoredValue::Response.new(@response_stub, "stadis_charge")
-      response.result[:status_code] = -1
+      response = BypassStoredValue::Response.new(@failed_response_stub, "stadis_account_charge")
       response.successful?.should == false
     end
 
     it "should return false for nil responses" do
-      response = BypassStoredValue::Response.new(nil, "stadis_charge")
+      response = BypassStoredValue::Response.new(nil, "stadis_account_charge")
       response.successful?.should == false
     end
   end

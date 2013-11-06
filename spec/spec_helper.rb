f66890798ae868cb1fde3c6fb106de91bdc43217
Ben Bean <bbean86@gmail.com>
2013-11-06 10:57:38 -0600

    Added after hook to disable webmock so we can send coverage stats to code climate from circle ci

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -10,6 +10,9 @@ require 'bypass_stored_value'
 
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 RSpec.configure do |config|
+  config.after(:suite) do
+    WebMock.disable!
+  end
   config.treat_symbols_as_metadata_keys_with_true_values = true
   config.run_all_when_everything_filtered = true
   config.filter_run :focus
